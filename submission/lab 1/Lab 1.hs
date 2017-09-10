fb6ddb66fa37ceb7e01867d61e75526a02e0b3f4
Michael de Lang <kingoipo@gmail.com>
2017-09-10 10:25:12 +0200

    Finishing touches, made sure it works when compiling and when using GHCi

--- submission/lab 1/Lab 1.hs
+++ submission/lab 1/Lab 1.hs
@@ -1,21 +1,17 @@
--- THIS IS NOT THE FINAL VERSION
-
 -- Niels Boerkamp
 -- Michael de Lang
--- Constatijn Bicker Caarten
+-- Constantijn Bicker Caarten
 -- Arjan Meijer
 
 -- Lab 1 exercises
 -- 05-09-2017
 
-
-module Lab1 where
 import Data.List
 import Test.QuickCheck
 
 prime :: Integer -> Bool
 prime n = n > 1 && all (\ x -> rem n x /= 0) xs
-  where xs = takeWhile (\ y -> y^2 <= n) primes
+    where xs = takeWhile (\ y -> y^2 <= n) primes
 
 primes :: [Integer]
 primes = 2 : filter prime [3..]
@@ -26,7 +22,7 @@ primesBelow x = takeWhile (< x) primes
 
 data NonNegativeSmall = NonNegativeSmall Integer deriving Show
 instance Arbitrary NonNegativeSmall where
-arbitrary = fmap NonNegativeSmall (choose (1, 10))
+    arbitrary = fmap NonNegativeSmall (choose (1, 10))
 
 -- Exercise 1
 -- We used the exercise of Niels Boerkamp because we thought that this one is
@@ -48,7 +44,7 @@ exercise1b :: Integer -> Bool
 exercise1b x = sumNumbersCubed x == ((x * (x + 1)) `div` 2) ^ 2
 
 -- Execution:
-ExerciseOne = do
+exerciseOne = do
     quickCheckResult (\(NonNegative n) -> exercise1a n)
     quickCheckResult (\(NonNegative n) -> exercise1b n)
 
@@ -56,11 +52,7 @@ ExerciseOne = do
 -- We used the exercise of Michael because we thought that this one is the easiest to read
 -- there were no mayor differences between our versions
 
-data NonNegativeSmall = NonNegativeSmall Integer deriving Show
-instance Arbitrary NonNegativeSmall where
-arbitrary = fmap NonNegativeSmall (choose (1, 25))
-
-ExerciseTwo = do quickCheckResult (\(NonNegativeSmall x) -> 2^(length [1..x]) == length (subsequences [1..x]))
+exerciseTwo = do quickCheckResult (\(NonNegativeSmall x) -> 2^(length [1..x]) == length (subsequences [1..x]))
 
 -- Making the generator generate lists of size [1..25] means that there'll be redundant tests.
 -- Making the generated lists larger than this leads to exponential times, after 25 my machine simply takes too long. This is due to powersets by definition creating exponential results. Permutating over that simply requires a lot of computational power.
@@ -73,7 +65,7 @@ ExerciseTwo = do quickCheckResult (\(NonNegativeSmall x) -> 2^(length [1..x]) ==
 -- https://stackoverflow.com/questions/13354726/using-a-custom-generator-vs-arbitrary-instance-in-quickcheck
 
 -- Exercise 3
--- We used the exercise of Constatijn because we thought this exercise used the most efficient and compact calculation method for the factorial
+-- We used the exercise of Constantijn because we thought this exercise used the most efficient and compact calculation method for the factorial
 
 -- This is hard to test, because this requires a lot of computation for large
 -- powersets since its length grows exponential. This checks a mathimatical
@@ -83,30 +75,30 @@ factorial :: Int -> Int
 factorial n = product [1..n]
 
 -- Custom test data set with range [1-20]
-data PositiveSmaller = PositiveSmaller Int deriving Show
-instance Arbitrary PositiveSmaller where
-arbitrary = fmap PositiveSmaller (choose (1, 10))
+data PositiveSmall = PositiveSmall Int deriving Show
+instance Arbitrary PositiveSmall where
+   arbitrary = fmap PositiveSmall (choose (1, 20))
 
-ExerciseThree = do quickCheckResult (\(PositiveSmall n) -> length (subsequences [1..n]) == 2^n)
+exerciseThree = do quickCheckResult (\(PositiveSmall n) -> length (subsequences [1..n]) == 2^n)
 
 -- Exercise 4
 
--- We used the exercise of Constatijn because he did not only test and create the findReversalPrimes, but he also tested the reversal method itself
+-- We used the exercise of Constantijn because he did not only test and create the findReversalPrimes, but he also tested the reversal method itself
 
 -- Reversal check
 -- Reversal fails for negative numbers and numbers ending in zeros, but
 -- there are no primes with this property so this poses no problem for
 -- finding primes which reversed are primes as well.
 
-reversal :: Int -> Int
+reversal :: Integer -> Integer
 reversal = read . reverse. show
 
-findReversalPrimes :: Int -> [Int]
+findReversalPrimes :: Integer -> [Integer]
 findReversalPrimes n = map reversal (filter prime (map reversal (takeWhile (<n) primes)))
 
-ExerciseFour = do
-    putStrLn $ show $ findReversalPrimes 10000
-	quickCheckResult(\(NonNegative n) -> n == reversal (reversal n))
+exerciseFour = do
+    putStrLn $ show $ findReversalPrimes 10000;
+    quickCheckResult(\(NonNegative n) -> n == reversal (reversal n))
 
 -- Exercise 5
 -- We used the exercise of Michael because he created a single function that solves this exercise,
@@ -114,13 +106,13 @@ ExerciseFour = do
 sumSmallestConsecutivePrimes :: Int -> Int -> [Integer]
 sumSmallestConsecutivePrimes x s | prime (sum consecutivePrimes) = (sum consecutivePrimes) : consecutivePrimes
                                  | otherwise = sumSmallestConsecutivePrimes x (s+1)
-                                 where consecutivePrimes = (take x (drop s primes)) -- Kan de where ook achter de eerste regel? dan wordt deze clausule niet onnodig uitgevoerd ?en is netter? (weet ik ook niet zeker)
+                                 where consecutivePrimes = (take x (drop s primes))
 
 -- output is a list with 102 elements, the 0th element being the answer and every element after it are the primes used to sum to get to this answer.
-ExerciseFive = do
+exerciseFive = do
     putStrLn $ show $ sumSmallestConsecutivePrimes 101 0
 
--- The function itself does not need to be tested since it will find a answer that statisiefs the given formula.
+-- The function itself does not need to be tested since it will find a answer that satisfies the given formula.
 -- Although we can check the result of the funcion if we test it on a pre-determined data set.
 -- An method could be:
 -- I.   The given are primes and should be consecutive
@@ -142,13 +134,13 @@ ExerciseFive = do
 -- END ORIGIONAL COMMENT
 
 -- Exercise 6
--- We used the exercise of Constatijn because he wrote the most compact code, there were no big differences in our answers
+-- We used the exercise of Constantijn because he wrote the most compact code, there were no big differences in our answers
 -- The smallest counter example are the first 6 consecutive prime numbers.
 
 consecutivePrimesProductPlusOne :: Int -> Integer
 consecutivePrimesProductPlusOne n = product (take n primes) + 1
 
-ExerciseSix = do
+exerciseSix = do
     quickCheckResult (\(NonNegative n) -> prime (consecutivePrimesProductPlusOne n))
 
 -- Exercise 7
@@ -178,43 +170,43 @@ toInt = foldl addDigit 0 where addDigit num d = 10*num + d
 
 isAmericanExpress :: Integer -> Bool
 isAmericanExpress x = (firstTwo == 34 || firstTwo == 37) && length digits == 15 && luhn digits where
-  digits = toDigits x
-  firstTwo = toInt $ take 2 digits
+    digits = toDigits x
+    firstTwo = toInt $ take 2 digits
 
 isMaster :: Integer -> Bool
 isMaster x = ((firstTwo >= 51 && firstTwo <= 55) || (firstFour >= 2221 && firstFour <=2720)) && length digits == 16 && luhn digits where
-  digits = toDigits x
-  firstTwo = toInt $ take 2 digits
-  firstFour = toInt $ take 4 digits
+    digits = toDigits x
+    firstTwo = toInt $ take 2 digits
+    firstFour = toInt $ take 4 digits
 
 isVisa :: Integer -> Bool
 isVisa x = digits !! 0 == 4 && (length digits == 13 || length digits == 16 || length digits == 19) && luhn digits
-  where digits = toDigits x
+    where digits = toDigits x
 
 checkValidity checker numbers unexpected name = do
-  let values = map checker numbers
-  if elem unexpected values then
-    do
-      putStrLn $ show $ filter (\(x,y) -> x == unexpected) $ zip values numbers
-      putStrLn ("Error in " ++ name)
-  else
-    putStrLn (name ++ " passed test")
+    let values = map checker numbers
+    if elem unexpected values then
+        do
+            putStrLn $ show $ filter (\(x,y) -> x == unexpected) $ zip values numbers
+            putStrLn ("Error in " ++ name)
+    else
+        putStrLn (name ++ " passed test")
 
 
-ExerciseSeven = do
-  let amExNumbers = [373880337659522,377661975930391,370701162614742,349352372863923,375684547926772,349813813922412,340801991391970,347227713429919,377693123451798,378370472971119,372792467814994,374347704665671,373796125847907,373719505757470,371983315150506,372442925729802,378761088197364,375448726177957,347037371661020,342854717755257,373306288377625,342211585475577,343235419902463,376546687823888,340273064581244,342450283948119,342256954114844,349115366366777,377654754141546,378606600233009,341307706114870,343380872740677,348812969536753,378306161052167,371838447299995,373674236619392,345655746263164,348286707091050,373836774757443,346149459313811,342415344945040,379602483932501,341312452516401,343129448230451,376189975125680,343679590775703,349819269009091,374955112234599,347614146045117,370740738438046,373373375175296,349817498043535,373117442625498,371264245645604,372503512233761,349160815410628,342008489695155,379950254675316,371341176890159,378850151972538,346763626907725,347126422452404,375389351543958,343082305362141,376780417994848,378375396294720,346421566536380,373491080805385,375991363129153,349902146247439,376165651713619,379445005082623,375195359659781,340112952438960,340323978393929,347341468881859,346221984975826,343358339513713,343463751856754,371350242042570,340790570449921,372773589068289,343917107175342,343888156173233,374769396202994,371448425333707,378831860414798,377375345776612,341936721521436,344422450175139,348092939556807,342247682049634,378865138661095,378509755088006,378315008336071,343464878634934,347314787271557,345195608810646,343823260827218,375648708635522]
-  let masterNumbers = [5555555555554444, 5437291847566632, 5531580388608737, 5145693648201538, 5206784260722632]
-  let visaNumbers = [4111111111111111, 4271706756598344, 4485585424322309, 4716463000295458, 4929165220142104]
-  let wrongAmExNumbers = [x*100000000000000 | x <- [1..99], x /= 34 && x /= 37]
-  let wrongMasterNumbers = [x*100000000000000 | x <- [1..99], not ((x >= 21 && x <= 27) || (x >= 51 && x <= 55))]
-  let wrongVisaNumbers = [x*100000000000000 | x <- [1..99], x /= 4 && x /= 42]
+exerciseSeven = do
+    let amExNumbers = [373880337659522,377661975930391,370701162614742,349352372863923,375684547926772,349813813922412,340801991391970,347227713429919,377693123451798,378370472971119,372792467814994,374347704665671,373796125847907,373719505757470,371983315150506,372442925729802,378761088197364,375448726177957,347037371661020,342854717755257,373306288377625,342211585475577,343235419902463,376546687823888,340273064581244,342450283948119,342256954114844,349115366366777,377654754141546,378606600233009,341307706114870,343380872740677,348812969536753,378306161052167,371838447299995,373674236619392,345655746263164,348286707091050,373836774757443,346149459313811,342415344945040,379602483932501,341312452516401,343129448230451,376189975125680,343679590775703,349819269009091,374955112234599,347614146045117,370740738438046,373373375175296,349817498043535,373117442625498,371264245645604,372503512233761,349160815410628,342008489695155,379950254675316,371341176890159,378850151972538,346763626907725,347126422452404,375389351543958,343082305362141,376780417994848,378375396294720,346421566536380,373491080805385,375991363129153,349902146247439,376165651713619,379445005082623,375195359659781,340112952438960,340323978393929,347341468881859,346221984975826,343358339513713,343463751856754,371350242042570,340790570449921,372773589068289,343917107175342,343888156173233,374769396202994,371448425333707,378831860414798,377375345776612,341936721521436,344422450175139,348092939556807,342247682049634,378865138661095,378509755088006,378315008336071,343464878634934,347314787271557,345195608810646,343823260827218,375648708635522]
+    let masterNumbers = [5555555555554444, 5437291847566632, 5531580388608737, 5145693648201538, 5206784260722632]
+    let visaNumbers = [4111111111111111, 4271706756598344, 4485585424322309, 4716463000295458, 4929165220142104]
+    let wrongAmExNumbers = [x*100000000000000 | x <- [1..99], x /= 34 && x /= 37]
+    let wrongMasterNumbers = [x*100000000000000 | x <- [1..99], not ((x >= 21 && x <= 27) || (x >= 51 && x <= 55))]
+    let wrongVisaNumbers = [x*100000000000000 | x <- [1..99], x /= 4 && x /= 42]
 
-  checkValidity isAmericanExpress amExNumbers False "American Express Valid Values"
-  checkValidity isMaster masterNumbers False "Mastercard Valid Values"
-  checkValidity isVisa visaNumbers False "Visa Valid Values"
-  checkValidity isAmericanExpress wrongAmExNumbers True "American Express Invalid Values"
-  checkValidity isMaster wrongMasterNumbers True "Mastercard Invalid Values"
-  checkValidity isVisa wrongVisaNumbers True "Visa Invalid Values"
+    checkValidity isAmericanExpress amExNumbers False "American Express Valid Values"
+    checkValidity isMaster masterNumbers False "Mastercard Valid Values"
+    checkValidity isVisa visaNumbers False "Visa Valid Values"
+    checkValidity isAmericanExpress wrongAmExNumbers True "American Express Invalid Values"
+    checkValidity isMaster wrongMasterNumbers True "Mastercard Invalid Values"
+    checkValidity isVisa wrongVisaNumbers True "Visa Invalid Values"
 
 -- I took the Haskell premaster with the Programming in Haskell book, so I already had the luhn function readily available.
 -- Some guy put the answers on github: https://github.com/adrianwong/programming-in-haskell-2nd-edition/blob/master/ch07_solutions.hs
@@ -246,7 +238,7 @@ guilty, honest :: [Boy]
 guilty = [ x | x <- boys, length (accusers x) == 3]
 honest = [ x | x <- boys, accuses x (head guilty)]
 
-ExerciseEight = do
+exerciseEight = do
     putStrLn $ show $ guilty
     putStrLn $ show $ honest
 
@@ -254,33 +246,33 @@ ExerciseEight = do
 -- Honest: Matthew, Peter & Carl
 
 -- Euler 9
--- We used the exercise of Constatijn because he had the most efficient code
+-- We used the exercise of Constantijn because he had the most efficient code
 -- Based on: https://stackoverflow.com/questions/7261667/pythagorean-triple-in-haskell-without-symmetrical-solutions/33727703#33727703
 pythagoreanTriples :: Int -> [(Int, Int, Int)]
 pythagoreanTriples n = [(a, b, c) | c <- [1..(div n 2)], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2 && a + b + c == n]
 
-EulerNine = do pythagoreanTriples 1000
+eulerNine = do putStrLn $ show $ pythagoreanTriples 1000
 
 -- Euler 10
--- We used the exercise of Niels because we prevered the useage of 'primesBelow' over 'takeWhile'
-EulerTen = do
+-- We used the exercise of Niels because we preferred the useage of 'primesBelow' over 'takeWhile'
+eulerTen = do
     putStrLn $ show $ sum (primesBelow 2000000)
 
 -- Euler 49
 -- We used the exercise of Michael because Michael was the only one who made this exercise
-EulerFourtyNine = do
-  putStrLn $ show $ [(x, x + 3330, x + 6660) | x <- (primesBelow 4000), elem (toDigits (x + 3330)) (permutations (toDigits x)), elem (toDigits (x + 6660)) (permutations (toDigits x)), prime (x + 3330), prime (x + 6660)]
+eulerFourtyNine = do
+    putStrLn $ show $ [(x, x + 3330, x + 6660) | x <- (primesBelow 4000), elem (toDigits (x + 3330)) (permutations (toDigits x)), elem (toDigits (x + 6660)) (permutations (toDigits x)), prime (x + 3330), prime (x + 6660)]
 -- This gives back two results: one tuple that is given in the problem and another tuple that is the other sequence the problem is talking about. Concatenated, this gives 296962999629.
 
 main = do
-    ExerciseOne
-	ExerciseTwo
-	ExerciseThree
-	ExerciseFour
-	ExerciseFive
-	ExerciseSix
-	ExerciseSeven
-	ExerciseEight
-	EulerNine
-	EulerTen
-	EulerFourtyNine
+    exerciseOne
+    exerciseTwo
+    exerciseThree
+    exerciseFour
+    exerciseFive
+    exerciseSix
+    exerciseSeven
+    exerciseEight
+    eulerNine
+    eulerTen
+    eulerFourtyNine
