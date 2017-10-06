053937e313efc9e2fee4f6f5f3c621398509b00e
Niels Boerkamp <n.boerkamp@outlook.com>
2017-10-06 21:52:38 +0200

    Litlle changes to Constantijn's work.

--- bicker/lab5.4.hs
+++ bicker/lab5.4.hs
@@ -8,21 +8,10 @@ import Lecture5 hiding (main)
 
 -- Exercise 4 (30 minutes)
 
--- sample1 xs = do
---   let l = length xs - 1
---   idx <- randomRIO (0, l)
---   return $ xs !! idx
---
--- sample 0 xs = return []
--- sample n xs = do
---   let l = min n (length xs)
---   val <- sample1 xs
---   (:) <$> (pure val) <*> (sample (l-1) (delete val xs))
-
 genEmptyBlocksProblem :: Node -> IO Node
 genEmptyBlocksProblem n = do
-    -- let xs = take 3 $ randomize [(1,1),(1,4),(1,7),(4,1),(4,4),(4,7),(7,1),(7,4),(7,7)]
-    let xs = [(1,1),(4,4),(7,7)]
+    y <- randomize [(1,1),(1,4),(1,7),(4,1),(4,4),(4,7),(7,1),(7,4),(7,7)]
+    let xs = take 3 $ y
     let eb = [(r,c) | x <- xs, r <- (bl $ fst x), c <- (bl $ snd x)]
     return (emptyPositions n eb)
 
@@ -35,4 +24,4 @@ main = do [r] <- rsolveNs [emptyN]
           showNode r
           s  <- genEmptyBlocksProblem r
           showNode s
-        --   showNode $ head $ solveNs [s]
+          showNode $ head $ solveNs [s]
